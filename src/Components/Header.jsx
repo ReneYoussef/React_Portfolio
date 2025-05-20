fdd8f1c8c42e7d0f517852cb5e19fdd72b9e3987
Rene A Youssef <Reneabdo10@gmail.com>
2025-05-20 18:38:58 +0300

    React Portfolio update services style and adding some hover effects

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,10 +10,10 @@ export default function Header() {
     <h2 className="header-text">Rene Youssef</h2>
     </div>
     <div className="nav-links">
-      <a href=".src/Components/AboutMe.jsx">About</a>
-      <a href="">Contact Us</a>
-      <a href="">Categories</a>
-      <a href="">Cart</a>
+      <a href="/AboutMe">About</a>
+      <a href="/Services">Services</a>
+      <a href="">Skills</a>
+     
       <button>Lets Talk</button>
     </div>
     
--- src/Components/Services/ServiceLists.jsx
+++ src/Components/Services/ServiceLists.jsx
@@ -2,11 +2,11 @@
 
 function ServiceLists({ services }) {
   return (
-    <div className="container">
+    <div className="Services-container">
       {services.map((service) => (
-        <div key={service.id} className="journal-entry">
-          <h3 className="entry-title">{service.title}</h3>
-          <p className="entry-text">{service.description}</p>
+        <div key={service.id} className="services">
+          <h3 className="services-title">{service.title}</h3>
+          <p className="services-text">{service.description}</p>
           
         </div>
       ))}
