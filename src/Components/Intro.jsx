d8c0e3d66f510a663bc0aa9e44a7c6303d51f175
Rene A Youssef <Reneabdo10@gmail.com>
2025-05-21 11:14:16 +0300

    React Portfolio

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -7,21 +7,32 @@ export default function Intro() {
         <img src={me} alt="About Image" />
       </div>
       <div className="AboutMe-text">
-        <h3>Hi, I'am <span> Rene Youssef</span> </h3>
-        
+        <h3>
+          Hi, I'am <span> Rene Youssef</span>{" "}
+        </h3>
+
         <h4>Photographer, Websites & Mobile Developer</h4>
       </div>
       <div className="AboutMe-Intro">
-        <p>I’m driven by the challenge of turning complex problems into simple, elegant solutions that people enjoy using every day.
-           When I’m not coding, you’ll find me hiking and capturing moments with my camera — a reminder to always stay curious and creative.</p>
-      
+        <p>
+          I’m driven by the challenge of turning complex problems into simple,
+          elegant solutions that people enjoy using every day. When I’m not
+          coding, you’ll find me hiking and capturing moments with my camera — a
+          reminder to always stay curious and creative.
+        </p>
       </div>
 
       <div className="AboutMe-info">
-<h2 className="Info-Number">1</h2>
-<span className="Info-Text">Years of Experience</span>
-<h2 className="Info-Number">3</h2>
-<span className="Info-Text">Projects Done</span>
+        <h2 className="Info-Number">1</h2>
+        <span className="Info-Text">Years of Experience</span>
+        <h2 className="Info-Number">3</h2>
+        <span className="Info-Text">Projects Done</span>
+      </div>
+
+
+      <div className="About_buttons">
+        <button type="submit">Download Cv</button>
+        <button>Gallery</button>
       </div>
     </div>
   );
